1eb6c97f6780d765b0bc6e0d28727add4fd1b5e8
Aniket Katkar <locked.ani@gmail.com>
2018-04-17 10:02:44 +0530

    Added Gallery from URL using Glide

--- app/src/main/java/com/aniketkatkar/eventswale/GalleryFragment.java
+++ app/src/main/java/com/aniketkatkar/eventswale/GalleryFragment.java
@@ -1,12 +1,19 @@
 package com.aniketkatkar.eventswale;
 
 
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.support.v4.view.ViewPager;
+import android.support.v7.widget.GridLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+
+import com.bumptech.glide.Glide;
 
 
 /**
@@ -14,21 +21,96 @@ import android.view.ViewGroup;
  */
 public class GalleryFragment extends Fragment {
 
+    RecyclerView recyclerView;
+    RecyclerView.LayoutManager layoutManager;
+    GalleryFragment.ImageGalleryAdapter adapter;
+
 
     public GalleryFragment() {
         // Required empty public constructor
     }
 
     @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-    }
-
-    @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_gallery, container, false);
+        View view = inflater.inflate(R.layout.fragment_gallery, container, false);
+
+        layoutManager = new GridLayoutManager(getActivity(), 2);
+        recyclerView = (RecyclerView) view.findViewById(R.id.rv_images);
+        recyclerView.setHasFixedSize(true);
+        recyclerView.setLayoutManager(layoutManager);
+
+        adapter = new GalleryFragment.ImageGalleryAdapter(getActivity(), SpacePhoto.getSpacePhotos());
+        recyclerView.setAdapter(adapter);
+
+        return view;
+    }
+
+    private class ImageGalleryAdapter extends RecyclerView.Adapter<ImageGalleryAdapter.MyViewHolder>  {
+
+        @Override
+        public ImageGalleryAdapter.MyViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+
+            Context context = parent.getContext();
+            LayoutInflater inflater = LayoutInflater.from(context);
+
+            // Inflate the layout
+            View photoView = inflater.inflate(R.layout.item_photo, parent, false);
+
+            ImageGalleryAdapter.MyViewHolder viewHolder = new ImageGalleryAdapter.MyViewHolder(photoView);
+            return viewHolder;
+        }
+
+        @Override
+        public void onBindViewHolder(ImageGalleryAdapter.MyViewHolder holder, int position) {
+
+            SpacePhoto spacePhoto = mSpacePhotos[position];
+            ImageView imageView = holder.mPhotoImageView;
+
+            Glide.with(mContext)
+                    .load(spacePhoto.getUrl())
+                    .placeholder(R.drawable.ic_cloud_off_red)
+                    .into(imageView);
+        }
+
+        @Override
+        public int getItemCount() {
+            return (mSpacePhotos.length);
+        }
+
+        public class MyViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener {
+
+            public ImageView mPhotoImageView;
+
+            public MyViewHolder(View itemView) {
+
+                super(itemView);
+                mPhotoImageView = (ImageView) itemView.findViewById(R.id.iv_photo);
+                itemView.setOnClickListener(this);
+            }
+
+            @Override
+            public void onClick(View view) {
+
+                int position = getAdapterPosition();
+                if(position != RecyclerView.NO_POSITION) {
+                    SpacePhoto spacePhoto = mSpacePhotos[position];
+
+                    Intent intent = new Intent(mContext, SpacePhotoActivity.class);
+                    intent.putExtra(SpacePhotoActivity.EXTRA_SPACE_PHOTO, spacePhoto);
+                    startActivity(intent);
+                }
+            }
+        }
+
+        private SpacePhoto[] mSpacePhotos;
+        private Context mContext;
+
+        public ImageGalleryAdapter(Context context, SpacePhoto[] spacePhotos) {
+            mContext = context;
+            mSpacePhotos = spacePhotos;
+        }
     }
 
 }
